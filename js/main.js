618b2996d6cb30ead440f8755cc4b0bbdd77d4d2
Eddie <53879350+Eddie344@users.noreply.github.com>
2020-01-06 17:42:53 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -9,4 +9,11 @@ $(function() {
 		},
 
 	});
+	$('textarea').each(function () {
+	  	this.setAttribute('style', 'height:' + (this.scrollHeight) + 'px;overflow-y:hidden;');
+		}).on('input', function () {
+			this.style.height = 'auto';
+			this.style.height = (this.scrollHeight) + 'px';
+			$(this).parent().attr('style', 'height:' + (this.style.height));
+		});
 });
